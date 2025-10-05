6c18e4a6aca724dddcb7e0fc4f1403e147b96cd7
Reuben <90466947+reubenzzz@users.noreply.github.com>
2025-10-05 14:01:12 +0530

    Update Auth.tsx

--- src/pages/Auth.tsx
+++ src/pages/Auth.tsx
@@ -50,7 +50,7 @@ const Auth = () => {
               <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M3.055 11H5a2 2 0 012 2v1a2 2 0 002 2 2 2 0 012 2v2.945M8 3.935V5.5A2.5 2.5 0 0010.5 8h.5a2 2 0 012 2 2 2 0 104 0 2 2 0 012-2h1.064M15 20.488V18a2 2 0 012-2h3.064M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
             </svg>
           </div>
-          <CardTitle className="text-2xl font-bold">NASA TEMPO</CardTitle>
+          <CardTitle className="text-2xl font-bold">Air-o-Pulse</CardTitle>
           <CardDescription>Air Quality Monitoring System</CardDescription>
         </CardHeader>
 
